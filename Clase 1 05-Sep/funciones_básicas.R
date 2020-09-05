7d1e43c7374e63384be1e3e5afc841c118fd4a0a
Gustavo Ríos Páez <ktavo@ktavo.ubuntu>
2020-09-05 11:48:16 -0300

    Basics init class 05-sep

---
+++ Clase 1 05-Sep/funciones_básicas.R
@@ -0,0 +1,18 @@
+#Función paste
+
+paste("Pega", "estas", 4, "palabras", sep = " ")
+
+A <- 2
+B <- 55
+C <- 3.2
+
+paste(A, B, C, sep = "**")
+
+#Paste0 pega caracteres sin separador
+paste0(A,B,C)
+
+#Listas
+1:5
+#Función sum con lista del 1 al 5
+sum(1:5)
+
